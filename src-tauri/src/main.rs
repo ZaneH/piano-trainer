c6fefd55200567a67de5b6e1663753db5df06894
Zane Helton <zane99@me.com>
2022-07-16 13:38:41 -0400

    Improve safety of MIDI calls

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -22,14 +22,25 @@ struct MidiMessage {
 
 #[tauri::command]
 fn list_midi_connections() -> HashMap<usize, String> {
-  let midi_in = MidiInput::new("piano-trainer-input").unwrap();
-  let mut devices = HashMap::new();
-  println!("Available input ports:");
-  for (i, p) in midi_in.ports().iter().enumerate() {
-    println!("{}: {}", i, midi_in.port_name(p).unwrap());
-    devices.insert(i, midi_in.port_name(p).unwrap());
+  let midi_in = MidiInput::new("piano-trainer-input");
+  match midi_in {
+    Ok(midi_in) => {
+      let mut midi_connections = HashMap::new();
+      for (i, p) in midi_in.ports().iter().enumerate() {
+        let port_name = midi_in.port_name(p);
+        match port_name {
+          Ok(port_name) => {
+            midi_connections.insert(i, port_name);
+          }
+          Err(e) => {
+            println!("Error getting port name: {}", e);
+          }
+        }
+      }
+      midi_connections
+    }
+    Err(_) => HashMap::new(),
   }
-  return devices;
 }
 
 #[tauri::command]
@@ -39,36 +50,47 @@ fn open_midi_connection(
   input_idx: usize,
 ) {
   let handle = Arc::new(window).clone();
-  let mut midi_in = MidiInput::new("piano-trainer-input").unwrap();
-  midi_in.ignore(Ignore::None);
-  let midi_in_ports = midi_in.ports();
-  *midi_state.input.lock().unwrap() = None;
-
-  if let Some(in_port) = midi_in_ports.get(input_idx) {
-    let conn_in = midi_in
-      .connect(
-        in_port,
-        "midir",
-        move |stamp, message, _log| {
-          // The last of the three callback parameters is the object that we pass in as last parameter of `connect`.
-
-          handle
-            .emit_all(
-              "midi_message",
-              MidiMessage {
-                message: message.to_vec(),
-              },
-            )
-            .map_err(|err| println!("{:?}", err))
-            .ok();
-
-          println!("{}: {:?} (len = {})", stamp, message, message.len());
-        },
-        (),
-      )
-      .unwrap();
-
-    *midi_state.input.lock().unwrap() = Some(conn_in);
+  let midi_in = MidiInput::new("piano-trainer-input");
+  match midi_in {
+    Ok(mut midi_in) => {
+      midi_in.ignore(Ignore::None);
+      let midi_in_ports = midi_in.ports();
+      let port = midi_in_ports.get(input_idx);
+      match port {
+        Some(port) => {
+          let midi_in_conn = midi_in.connect(
+            port,
+            "midir",
+            move |_, message, _| {
+              handle
+                .emit_all(
+                  "midi_message",
+                  MidiMessage {
+                    message: message.to_vec(),
+                  },
+                )
+                .map_err(|e| {
+                  println!("Error sending midi message: {}", e);
+                })
+                .ok();
+            },
+            (),
+          );
+          match midi_in_conn {
+            Ok(midi_in_conn) => {
+              midi_state.input.lock().unwrap().replace(midi_in_conn);
+            }
+            Err(e) => {
+              println!("Error: {}", e);
+            }
+          }
+        }
+        None => {
+          println!("No port found at index {}", input_idx);
+        }
+      }
+    }
+    Err(e) => println!("Error: {}", e),
   }
 }
 
